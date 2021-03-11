f08aaef01fee3884795ceeca655d218dbb9c75d0
t0702 <t0702@naver.com>
2021-03-11 16:33:42 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,33 @@
 import React, { useEffect,useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './component/search_header/search_header';
 import VideoList from './component/video_list/video_list';
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    youtube
+      .search(query)
+      .then(videos => setVideos(videos));
+  };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBJjfZNow0c_zHxOYxc1wtcpxsDBhdx5sI", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular()
+      .then(videos => setVideos(videos));
   }, []);
+
   /* 
     [] 빈칸이면 한번만 업데이트, 
     아무것도 전달하지 않으면 컴포넌트의 state나 prop이 
     업데이트될때마다 무조건 반복적으로 호출 
   */ 
-  return <VideoList videos={videos} />
-  
+  return (
+    <div className={styles.app} >
+      <SearchHeader onSearch={search}/>
+      <VideoList videos={videos} />
+    </div>
+  )
 }
 
 export default App;
